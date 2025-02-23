01dc160b5735419183fe4b088527fa44b525aac9
Antonio Felix de Oliveira Neto <antoniofelixxx7@gmail.com>
2025-02-23 01:00:18 -0300

    Ajustando os sites

--- exercicio/02-ex-calculadora/script/main.js
+++ exercicio/02-ex-calculadora/script/main.js
@@ -1,9 +1,64 @@
-import operation from "./operation.js";
-import arrayResult from "./resultados.js";
-import put_inp_spn_txtResult from "./history.js";
-import fixString from "./fixString.js";
+/* 
+	Como coloquei no gitHub. E provavel que irão abrir file:// -> os imports não funcionará.
+	Teria que abrir o index.html em live server para ficar legal.
+	
+*/
+
+/*
+	import arrayResult from "./script/resultado.js"
+	import operation from "./script/operation.js"
+	import put_inp_spn_txtResult from "./script/history.js"
+	import fixString from "./script/fixString.js"
+
+*/
 
 const form = document.querySelector("#form");
+
+const arrayResult = ["...", "...", "...", "...", "..."];
+
+const operation = {
+	"+": (num1, num2) => num1 + num2,
+	"-": (num1, num2) => num1 - num2,
+	"*": (num1, num2) => num1 * num2,
+	"/": (num1, num2) => (num2 !== 0 ? num1 / num2 : "Error "),
+};
+
+const organizeArray = (txtResultado) => {
+	arrayResult.length < 5
+		? arrayResult.unshift(txtResultado)
+		: (arrayResult.pop(), arrayResult.unshift(txtResultado));
+};
+
+const allSpnResult = document.querySelectorAll(".spn-result");
+
+const put_inp_spn_txtResult = (arrayResult) => {
+	let indice = 0;
+	allSpnResult.forEach((item) => {
+		item.innerHTML = arrayResult[indice];
+		indice++;
+	});
+	/*
+	
+		li.1 -> 1 resultado 
+		li.2 -> 2 resultado 
+		li.3 -> 3 resultado 
+		li.4 -> 4 resultado 
+		li.5 -> 5 resultado. 
+	
+		input -> 10
+	
+		Novo resultado (10)
+	
+		li.1 -> Novo resultado (10)
+		li.2 -> resultado anterior do li.1
+		li.3 -> resultado anterior do li.2
+		li.4 -> resultado anterior do li.3
+		li.5 -> resultado anterior do li.4
+	*/
+};
+
+const fixString = (string) =>
+	string.length > 6 ? `${string.substring(0, 6)}.` : string;
 
 form.addEventListener("submit", (evt) => {
 	evt.preventDefault();
@@ -26,9 +81,3 @@ form.addEventListener("submit", (evt) => {
 	organizeArray(txt);
 	put_inp_spn_txtResult(arrayResult);
 });
-
-const organizeArray = (txtResultado) => {
-	arrayResult.length < 5
-		? arrayResult.unshift(txtResultado)
-		: (arrayResult.pop(), arrayResult.unshift(txtResultado));
-};
